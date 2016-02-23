5d651022a08f7831b336c42942c6026d9e4767d1
sennew <sennewens@hotmail.com>
2016-02-23 16:10:33 +0100

    // Toevoeging loginFragment && loginPresenter

--- app/src/main/java/com/projects/wens/kandoeteami/login/LoginContract.java
+++ app/src/main/java/com/projects/wens/kandoeteami/login/LoginContract.java
@@ -4,4 +4,12 @@ package com.projects.wens.kandoeteami.login;
  * Created by senne on 23/02/2016.
  */
 public interface LoginContract {
+    interface view{
+
+    }
+
+    interface UserActionListener{
+
+    }
+
 }
--- app/src/main/java/com/projects/wens/kandoeteami/login/LoginFragment.java
+++ app/src/main/java/com/projects/wens/kandoeteami/login/LoginFragment.java
@@ -1,7 +1,11 @@
 package com.projects.wens.kandoeteami.login;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by senne on 23/02/2016.
  */
-public class LoginFragment {
+public class LoginFragment extends Fragment implements LoginContract.view {
+
+
 }
