fe4c74cbda2e732811b9ced490ae52b381734d99
sennew <sennewens@hotmail.com>
2016-02-28 17:44:49 +0100

    //No sideMenu login & register

--- app/src/main/java/com/projects/wens/kandoeteami/login/LoginActivity.java
+++ app/src/main/java/com/projects/wens/kandoeteami/login/LoginActivity.java
@@ -1,19 +1,16 @@
 package com.projects.wens.kandoeteami.login;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.projects.wens.kandoeteami.R;
-import com.projects.wens.kandoeteami.organisation.ListOrganisationFragment;
 
 /**
  * Created by senne on 23/02/2016.
@@ -24,18 +21,10 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_noside);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
-
-        if (navigationView != null){
-            setupDrawerContent(navigationView);
-        }
 
         if (null == savedInstanceState){
             initFragment(LoginFragment.newInstance());
