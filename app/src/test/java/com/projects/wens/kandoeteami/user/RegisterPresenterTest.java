666468004ffa862ed3fafa747a2e99e2471dcf18
sennew <sennewens@hotmail.com>
2016-03-01 12:21:02 +0100

    //Aanpassen testen

---
+++ app/src/test/java/com/projects/wens/kandoeteami/user/RegisterPresenterTest.java
@@ -0,0 +1,83 @@
+package com.projects.wens.kandoeteami.user;
+
+import com.projects.wens.kandoeteami.register.RegisterContract;
+import com.projects.wens.kandoeteami.register.RegisterPresenter;
+import com.projects.wens.kandoeteami.retrofit.ServiceGenerator;
+import com.projects.wens.kandoeteami.retrofit.service.RegisterService;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by senne on 01/03/2016.
+ */
+public class RegisterPresenterTest {
+    @Mock
+    RegisterService mRegisterService;
+    @Mock
+    private RegisterContract.view mRegisterView;
+
+    private RegisterPresenter mRegisterPresenter;
+
+    @Before
+    public void setmRegisterPresenter(){
+        MockitoAnnotations.initMocks(this);
+        mRegisterService = ServiceGenerator.createService(RegisterService.class, "http://wildfly-teamiip2kdgbe.rhcloud.com/api");
+        mRegisterPresenter = new RegisterPresenter(mRegisterView, mRegisterService);
+    }
+
+    @Test
+    public void checkUsernameNotNull(){
+        when(mRegisterView.getUsername()).thenReturn("");
+        when(mRegisterView.getEmail()).thenReturn("sennewens@hotmail.com");
+        when(mRegisterView.getPassword()).thenReturn("hallo");
+        when(mRegisterView.getRetypePassword()).thenReturn("hallo");
+        mRegisterPresenter.register(null);
+        verify(mRegisterView).showUsernameError("Username is empty");
+    }
+
+    @Test
+    public void checkEmailNotNull(){
+        when(mRegisterView.getUsername()).thenReturn("sennew");
+        when(mRegisterView.getEmail()).thenReturn("");
+        when(mRegisterView.getPassword()).thenReturn("hallo");
+        when(mRegisterView.getRetypePassword()).thenReturn("hallo");
+        mRegisterPresenter.register(null);
+        verify(mRegisterView).showEmailError("Email is empty");
+    }
+
+    @Test
+    public void checkPasswordNotNull(){
+        when(mRegisterView.getUsername()).thenReturn("sennew");
+        when(mRegisterView.getEmail()).thenReturn("sennewens@hotmail.com");
+        when(mRegisterView.getPassword()).thenReturn("");
+        when(mRegisterView.getRetypePassword()).thenReturn("hallo");
+        mRegisterPresenter.register(null);
+        verify(mRegisterView).showPasswordError("Password is empty!");
+    }
+
+    @Test
+      public void checkRetypePasswordNotNull(){
+        when(mRegisterView.getUsername()).thenReturn("sennew");
+        when(mRegisterView.getEmail()).thenReturn("sennewens@hotmail.com");
+        when(mRegisterView.getPassword()).thenReturn("hallo");
+        when(mRegisterView.getRetypePassword()).thenReturn("");
+        mRegisterPresenter.register(null);
+        verify(mRegisterView).showRetypePasswordError("Passwords do not match!");
+    }
+
+    @Test
+    public void check2WrongPasswordsNotNull(){
+        when(mRegisterView.getUsername()).thenReturn("sennew");
+        when(mRegisterView.getEmail()).thenReturn("sennewens@hotmail.com");
+        when(mRegisterView.getPassword()).thenReturn("hallo2");
+        when(mRegisterView.getRetypePassword()).thenReturn("hallo");
+        mRegisterPresenter.register(null);
+        verify(mRegisterView).showRetypePasswordError("Passwords do not match!");
+    }
+}
