d529398c74daeda7978c0dcac696f82297322d17
sennew <sennewens@hotmail.com>
2016-03-06 00:58:59 +0100

    //Aanpassen detail organisationLayout

---
+++ app/src/main/java/com/projects/wens/kandoeteami/organisation/data/GroupHolder.java
@@ -0,0 +1,17 @@
+package com.projects.wens.kandoeteami.organisation.data;
+
+import android.widget.TextView;
+
+/**
+ * Created by senne on 05/03/2016.
+ */
+public class GroupHolder {
+    public TextView title;
+
+    public GroupHolder(){
+    }
+
+    public void setTitle(TextView title) {
+        this.title = title;
+    }
+}
