dcb0b7f3d79cce4e15dee26562f27c193947ed54
sennew <sennewens@hotmail.com>
2016-03-06 00:56:19 +0100

    //Aanpassen detail organisationLayout

--- app/src/main/java/com/projects/wens/kandoeteami/organisation/adapter/ExpandableListViewAdapter.java
+++ app/src/main/java/com/projects/wens/kandoeteami/organisation/adapter/ExpandableListViewAdapter.java
@@ -5,29 +5,82 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
 
+import com.projects.wens.kandoeteami.R;
+import com.projects.wens.kandoeteami.organisation.data.ChildHolder;
+import com.projects.wens.kandoeteami.organisation.data.ChildItem;
+import com.projects.wens.kandoeteami.organisation.data.GroupHolder;
 import com.projects.wens.kandoeteami.organisation.data.GroupItem;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by senne on 04/03/2016.
  */
-public class expandableListViewAdapter extends BaseExpandableListAdapter {
+public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
+    private static final String PICASSO_BASEURL = "http://wildfly-teamiip2kdgbe.rhcloud.com/";
     private LayoutInflater inflater;
     private List<GroupItem> items;
 
-    public expandableListViewAdapter(Context context){
+    public ExpandableListViewAdapter(Context context){
         inflater = LayoutInflater.from(context);
     }
 
-    public void setDate(List<GroupItem> items){
+    public void setData(List<GroupItem> items){
         this.items = items;
     }
 
     @Override
-    public Object getChild(int groupPosition, int childPosition) {
+    public ChildItem getChild(int groupPosition, int childPosition) {
         return items.get(groupPosition).children.get(childPosition);
+    }
+
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        ChildHolder holder;
+        ChildItem item = getChild(groupPosition, childPosition);
+        if (convertView == null){
+            holder = new ChildHolder();
+            convertView = inflater.inflate(R.layout.expandable_list_item, parent, false);
+            holder.firstName = (TextView) convertView.findViewById(R.id.textVoornaam);
+            holder.lastName = (TextView) convertView.findViewById(R.id.textAchternaam);
+            holder.role = (TextView) convertView.findViewById(R.id.textMemberOrganisator);
+            holder.profilePicture = (CircleImageView) convertView.findViewById(R.id.user_img_expandable);
+            convertView.setTag(holder);
+        } else{
+            holder = (ChildHolder) convertView.getTag();
+        }
+
+        holder.firstName.setText(item.firstName);
+        holder.lastName.setText(item.lastName);
+        holder.role.setText(item.role);
+        if (item.profilePictureUrl.charAt(0) == 'h'){
+            Picasso.with(convertView.getContext()).load(item.profilePictureUrl).into(holder.profilePicture);
+        } else{
+            Picasso.with(convertView.getContext()).load(PICASSO_BASEURL + item.profilePictureUrl).into(holder.profilePicture);
+        }
+        return convertView;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        GroupHolder holder;
+        GroupItem item = getGroup(groupPosition);
+        if (convertView == null){
+            holder = new GroupHolder();
+            convertView = inflater.inflate(R.layout.expandable_list_group, parent, false);
+            holder.title = (TextView) convertView.findViewById(R.id.expandable_title);
+            convertView.setTag(holder);
+        } else {
+            holder = (GroupHolder) convertView.getTag();
+        }
+        holder.title.setText(item.getTitle());
+        return convertView;
     }
 
     @Override
@@ -37,45 +90,33 @@ public class expandableListViewAdapter extends BaseExpandableListAdapter {
 
     @Override
     public int getGroupCount() {
-        return 0;
+        return items.size();
     }
 
     @Override
     public int getChildrenCount(int groupPosition) {
-        return 0;
+        return items.get(groupPosition).getChildren().size();
     }
 
     @Override
-    public Object getGroup(int groupPosition) {
-        return null;
+    public GroupItem getGroup(int groupPosition) {
+        return items.get(groupPosition);
     }
-
-
 
     @Override
     public long getGroupId(int groupPosition) {
-        return 0;
+        return groupPosition;
     }
 
 
 
     @Override
     public boolean hasStableIds() {
-        return false;
-    }
-
-    @Override
-    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        return null;
-    }
-
-    @Override
-    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        return null;
+        return true;
     }
 
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
-        return false;
+        return true;
     }
 }
